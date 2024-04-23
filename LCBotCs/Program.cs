d318569e77a2733c78dbabf80a4b1207efc13bd5
Carim A <carim@glaciate.net>
2024-04-23 11:20:44 +0100

    .env config

--- LCBotCs/Program.cs
+++ LCBotCs/Program.cs
@@ -1,10 +1,12 @@
-﻿using LCBotCs;
+﻿using DotNetEnv;
+using LCBotCs;
 using PsimCsLib;
 
+DotNetEnv.Env.Load();
 var options = new PsimClientOptions()
 {
-    Username = "",
-    Password = ""
+     Username = Environment.GetEnvironmentVariable("PSIM_USERNAME") ?? throw new EnvVariableNotFoundException("PSIM_USERNAME not found", nameof(PsimClientOptions)),
+     Password = Environment.GetEnvironmentVariable("PSIM_PASSWORD") ?? throw new EnvVariableNotFoundException("PSIM_PASSWORD not found", nameof(PsimClientOptions))
 };
 
 var client = new PsimClient(options);
