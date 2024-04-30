37db9bb85e0f875190da618372a648d725d132ff
Carim A <carim@glaciate.net>
2024-04-30 23:57:42 +0100

    Fix nickname issue

--- LcBotCsWeb/Modules/SampleTeams/TeamPreview.cs
+++ LcBotCsWeb/Modules/SampleTeams/TeamPreview.cs
@@ -25,13 +25,14 @@
     {
         return data
             .Split(new[] { "\r\n", "\n" }, StringSplitOptions.RemoveEmptyEntries)
-            .Where(line => !string.IsNullOrWhiteSpace(line.Trim()))
+            .Select(line => line.Trim())
+            .Where(line => !string.IsNullOrWhiteSpace(line))
             .Where(FilterNonNames)
             .Select(GetName)
             .ToList();
     }
 
-    private static bool FilterNonNames(string line) => !(line.Contains(':') || line.Contains('/') || line.Contains("Nature") || line.Contains("- "));
+    private static bool FilterNonNames(string line) => !(line.Contains(':') || line.Contains('/') || line.EndsWith("Nature") || line.StartsWith("- "));
     private static string GetName(string line)
     {
         var chunk = line.Split('@')[0].Replace("(M)", string.Empty).Replace("(F)", string.Empty);
