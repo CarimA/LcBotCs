af8b89f555eb1159b9d4b610874cedaa8958941e
Carim A <carim@glaciate.net>
2024-04-30 22:56:07 +0100

    Team HTML Formatter

--- LcBotCsWeb/Modules/SampleTeams/TeamHtmlFormatter.cs
+++ LcBotCsWeb/Modules/SampleTeams/TeamHtmlFormatter.cs
@@ -1,7 +1,17 @@
-﻿public class TeamHtmlFormatter
+﻿using System.Text;
+
+public class TeamHtmlFormatter
 {
-    public static string Generate(Team team)
+    public static string Generate(TeamPreview team)
     {
-        return string.Empty;
+        var icons = string.Join(string.Empty, team.Pokemon.OrderBy(x => x)
+            .Select(pokemon => $"<psicon pokemon='{pokemon}'>"));
+
+        return $"<div style='display: inline-flex; padding: 10px; box-sizing: border-box; align-items: center;'><div style='flex-shrink: 0;'>{icons}</div> <div><a style='font-size:120%;' href='{team.Link}'>{team.Title}</a><br><a style='font-size: 70%; text-decoration: none;' href='{team.Link}'>{team.Author} • {team.Link}</a></div></div>";
+    }
+
+    public static string Generate(List<TeamPreview> teams)
+    {
+        return string.Join(string.Empty, teams.Select(Generate));
     }
 }
