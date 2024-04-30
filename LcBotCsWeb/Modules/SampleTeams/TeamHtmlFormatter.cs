886852961e8ac57a175bd5e60366cfc48d10772f
Carim A <carim@glaciate.net>
2024-04-30 23:42:29 +0100

    Yet more formatting changes

--- LcBotCsWeb/Modules/SampleTeams/TeamHtmlFormatter.cs
+++ LcBotCsWeb/Modules/SampleTeams/TeamHtmlFormatter.cs
@@ -5,7 +5,7 @@
         var icons = string.Join(string.Empty, team.Pokemon.OrderBy(x => x)
             .Select(pokemon => $"<psicon pokemon='{pokemon}'>"));
 
-        return $"<div style='display: flex; padding: 8px; box-sizing: border-box;'><div style='margin-right: 8px;'><a style='text-decoration: none;' href='{team.Link}'>{icons}</a></div> <div><a style='font-size:100%;' href='{team.Link}'>{team.Title}</a><br><a style='font-size: 70%; text-decoration: none; letter-spacing: 0.75px;' href='{team.Link}'>{team.Author} • {team.Link}</a></div></div>";
+        return $"<div style='display: flex; padding: 8px; box-sizing: border-box;'><div style='margin-right: 8px;'><a style='text-decoration: none;' href='{team.Link}'>{icons}</a></div> <div><a style='font-size:100%;' href='{team.Link}'>{team.Title}</a><br><a style='font-size: 80%; text-decoration: none; letter-spacing: 0.65px;' href='{team.Link}'>{team.Author} • {team.Link}</a></div></div>";
     }
 
     public static string Generate(List<TeamPreview> teams)
