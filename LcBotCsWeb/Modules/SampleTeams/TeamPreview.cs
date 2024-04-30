c1b3cf59dfbbc18434fb1e8b6e4f423acb0484fe
Carim A <carim@glaciate.net>
2024-04-30 22:56:00 +0100

    Team object

---
+++ LcBotCsWeb/Modules/SampleTeams/TeamPreview.cs
@@ -0,0 +1,15 @@
+﻿public class TeamPreview
+{
+    public string Link { get; set; }
+    public string Author { get; set; }
+    public string Title { get; set; }
+    public List<string> Pokemon { get; set; }
+
+    public TeamPreview(string link, string author, string title, List<string> pokemon)
+    {
+        Link = link;
+        Author = author;
+        Title = title;
+        Pokemon = pokemon;
+    }
+}
