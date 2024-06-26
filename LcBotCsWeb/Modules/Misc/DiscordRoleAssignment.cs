8ac95683791abe11fd28ffe2a736a5d9455337a2
Carim A <carim@glaciate.net>
2024-06-26 15:51:59 +0100

    what has my life come to

--- LcBotCsWeb/Modules/Misc/DiscordRoleAssignment.cs
+++ LcBotCsWeb/Modules/Misc/DiscordRoleAssignment.cs
@@ -1,5 +1,6 @@
 ﻿using Discord;
 using Discord.Interactions;
+using Discord.WebSocket;
 
 namespace LcBotCsWeb.Modules.Misc;
 
@@ -55,5 +56,44 @@ public class DiscordRoleAssignment : InteractionModuleBase<SocketInteractionCont
 		await ToggleRole(MatchesRole,
 			"You have been assigned the matches role and will be notified of tournament matches.",
 			"You have been unassigned the matches role and will no longer be notified of tournament matches.");
+	}
+
+
+	private DateTime? _baldCheckCooldown;
+
+	[SlashCommand("baldcheck", "Check where on the Norwood Scale a user is")]
+	public async Task BaldCheck(SocketGuildUser user)
+	{
+		if (DateTime.UtcNow < _baldCheckCooldown)
+			await RespondAsync("On cooldown!", null, false, true);
+
+		_baldCheckCooldown = DateTime.UtcNow + TimeSpan.FromSeconds(5);
+
+		var id = user.Id;
+		var name = user.DisplayName;
+
+		if (id is 295323609243844608 or 203660036092854272 or 199994947225649153 or 242299071472074753)
+		{
+			await RespondAsync("The scale has broken, try as you might there is no oasis to be found in this desert of missing follicles. @ is sadly bald.".Replace("@", name));
+			await Task.Delay(TimeSpan.FromSeconds(3));
+			await FollowupAsync("Condolences.");
+
+			return;
+		}
+
+		var results = new List<string>()
+		{
+			"Norwood 1, Looking good, @",
+			"Norwood 2, Pretty workable, @",
+			"Norwood 3, @ is starting to bald",
+			"Norwood 3 vertex, Uh oh, @",
+			"Norwood 4, Time to consider giving up @",
+			"Norwood 5, Baldness is approaching, @",
+			"Norwood 6, @...",
+			"Norwood 7, you're basically bald at this point @"
+		};
+
+		var index = Math.Abs(id.GetHashCode() % results.Count);
+		await RespondAsync(results[index].Replace("@", name));
 	}
 }
