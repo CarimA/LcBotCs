c7d2dc612c98092af9dd98cae038f78bec1ef138
Carim A <carim@glaciate.net>
2024-07-13 19:06:21 +0100

    hotfix: display name

--- LcBotCsWeb/Modules/Bridge/PsimToDiscordBridge.cs
+++ LcBotCsWeb/Modules/Bridge/PsimToDiscordBridge.cs
@@ -35,7 +35,7 @@ public class PsimToDiscordBridge : ISubscriber<ChatMessage>
 			return;
 
 		var name = $"{PsimUsername.FromRank(msg.User.Rank)}{msg.User.DisplayName}".Trim();
-		var output = $"**[{msg.Room}] {name}:** {message}";
+		var output = $"**[{msg.Room.Name}] {name}:** {message}";
 		if (string.IsNullOrEmpty(output))
 			return;
 
